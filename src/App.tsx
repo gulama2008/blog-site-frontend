dd0819aac6ff667c7534a454f0a13bbaa6481d32
Siyu Liu <siyu.java.web@gmail.com>
2024-02-07 12:33:49 +1100

    sets react routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import BlogContextProvider from "./context/BlogContextProvider";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import BlogPage from "./pages/BlogPage/BlogPage";
 
 function App() {
-  return <BlogContextProvider>
-    
-  </BlogContextProvider>;
+  return (
+    <BlogContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/:id" element={<BlogPage />} />
+        </Routes>
+      </BrowserRouter>
+    </BlogContextProvider>
+  );
 }
 
 export default App;
