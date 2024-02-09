79c3cefe1f4895fc067eff7db993551382f55ba2
Siyu Liu <siyu.java.web@gmail.com>
2024-02-09 22:17:25 +1100

    adds filter for comments and adds more styles

--- src/containers/CommentsContainer/CommentsContainer.tsx
+++ src/containers/CommentsContainer/CommentsContainer.tsx
@@ -1,18 +1,55 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { BlogContext, CommentItem } from '../../context/BlogContextProvider';
 import CommentListItem from '../../components/CommentListItem/CommentListItem';
 import styles from "./CommentsContainer.module.scss"
 const CommentsContainer = () => {
-  const { comments, setComments } = useContext(BlogContext);
-
+  const { comments } = useContext(BlogContext);
+  const [commentsDisplay, setCommentsDisplay] = useState<CommentItem[]>(comments);
+  const handleChange = (e:any) => { 
+    if (e.target.value == "") {
+      setCommentsDisplay(comments);
+    } else if (e.target.value == "true") {
+      const blockedComments = comments.filter((comment: CommentItem) => {
+        return comment.blocked
+      })
+      setCommentsDisplay(blockedComments);
+    } else { 
+      const unblockedComments = comments.filter((comment: CommentItem) => {
+        return !comment.blocked;
+      });
+      setCommentsDisplay(unblockedComments);
+    }
+  }
   return (
     <div className={styles.container}>
-      <div className={styles.title}>All Comments</div>
-      {comments.map((comment:CommentItem,index:number) => { 
-        return <CommentListItem comment={comment} index={index} key={index}/>
-      })}
+      <div className={styles.header}>
+        <div className={styles.title}>All Comments</div>
+        <div className={styles.filter_container}>
+          <select
+            name=""
+            id=""
+            className={styles.filter}
+            onChange={handleChange}
+          >
+            <option value="">All comments</option>
+            <option value="true">Blocked comments</option>
+            <option value="false">Unblocked comments</option>
+          </select>
+        </div>
+      </div>
+      <div className={styles.comments}>
+        {commentsDisplay.map((comment: CommentItem) => {
+          return (
+            <CommentListItem
+              comment={comment}
+              index={comment.id}
+              key={comment.id}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default CommentsContainer
